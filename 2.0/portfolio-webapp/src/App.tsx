6311951adc545756b29e38666493677cf5fdb0c0
Nathan Coxson <nathancoxson2739@gmail.com>
2024-01-31 06:07:13 -0500

    removed hover effect from project cards

--- 2.0/portfolio-webapp/src/App.tsx
+++ 2.0/portfolio-webapp/src/App.tsx
@@ -8,16 +8,16 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='max-w-7xl mx-auto p-8 text-center flex-col justify-center content-center w-max'>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className='color' alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://react.dev" target="_blank" >
+          <img src={reactLogo} className='h-24' alt="React logo" />
         </a>
       </div>
       <h1 className=''>Vite + React</h1>
-      <div className="card">
+      <div className='p-8'>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-gray-400">
         Click on the Vite and React logos to learn more
       </p>
     </>
